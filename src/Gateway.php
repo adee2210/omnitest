5eb448215e428ca2b58a16c366ab768ab3064ece
Adee <adee_2210@hotmail.com>
2015-05-25 16:04:09 +0700

    PSR 4 replace tab with space

--- src/Gateway.php
+++ src/Gateway.php
@@ -12,107 +12,107 @@ class Gateway extends GatewayCore implements GatewayTemplate
         return $this->setConfig() && $this->setVars() && $this->setName();
     }
 
-	private function setName(){
-		return $this->name = 'Omnitest Gateway';
-	}
+    private function setName(){
+        return $this->name = 'Omnitest Gateway';
+    }
 
     private function setConfig($args = array())
     {
-		return $this->configs = array(
-			'ccPurchase' => array(
-				'args' => array(
-					'sid',
-					'rcode',
-					'country',
-					'addfee',
-					'cart',
-					'summary',
-					'currency_code',
-					'items' => array(
-						'name',
-						'quantity',
-						'amount_unit',
-						'item_no',
-						'item_desc',
-					)
-				),
+        return $this->configs = array(
+            'ccPurchase' => array(
+                'args' => array(
+                    'sid',
+                    'rcode',
+                    'country',
+                    'addfee',
+                    'cart',
+                    'summary',
+                    'currency_code',
+                    'items' => array(
+                        'name',
+                        'quantity',
+                        'amount_unit',
+                        'item_no',
+                        'item_desc',
+                    )
+                ),
                 'description' => 'Allows you to test Payment gateway.',
                 'url' => 'http://www.adeeisme.com/omnitest/api/',//'http://localhost/ws/api/',
                 'txnMode' => 'ccPurchase',
                 'optional' => array(
-					'addfee',
+                    'addfee',
                 ),
-				'format' => array(
-					'send' => 	'json',
-					'receive' => 'json',
-				),
+                'format' => array(
+                    'send' =>     'json',
+                    'receive' => 'json',
+                ),
                 'response' => 'ccTxnResponseV1'
-			)
-		);
+            )
+        );
     }
 
     private function setVars($args = array())
     {
-		return $this->vars = array(
+        return $this->vars = array(
             'form'=>array(
-				'sid' => array(
-					'type' => 'text',
-					'description' => 'Site id (unique identifier for the web site)',
-				),
-				'rcode' => array(
-					'type' => 'text',
-					'description' => 'Site RCODE',
-				),
-				'country' => array(
-					'type' => 'text',
-					'description' => 'Country of the user - will provide the list of payments
-	available for that country',
-				),
-				'addfee' => array(
-					'type' => 'select',
-					'default' => array( true => 'True', false => 'False'),
-					'description' => 'This will add the processing fees on top of the amount.',
-				),
-				'cart' => array(
-					'type' => 'text',
-					'description' => 'Information about the purchase items. This is only
-	compulsory for feeapi, do not sent the cart to rawfeeapi.',
-				),
-				'summary' => array(
-					'type' => 'text',
-					'description' => 'Contains a summary of cart contents',
-				),
-				'currency_code' => array(
-					'type' => 'text',
-					'description' => '3 digit currency code',
-				),
-				'items' => array(
-					'type' => 'array',
-					'description' => 'Contains the cart items',
-				),
-				'name' => array(
-					'type' => 'text',
-					'description' => 'Category, can be used freely',
-				),
-				'quantity' => array(
-					'type' => 'text',
-					'description' => 'Quantity of the item',
-				),
-				'amount_unit' => array(
-					'type' => 'text',
-					'description' => 'Unit price amount (without commas and only 2 decimals
-	places)',
-				),
-				'item_no' => array(
-					'type' => 'text',
-					'description' => 'Article number',
-				),
-				'item_desc' => array(
-					'type' => 'text',
-					'description' => 'Item description',
-				),
-			)
-		);
+                'sid' => array(
+                    'type' => 'text',
+                    'description' => 'Site id (unique identifier for the web site)',
+                ),
+                'rcode' => array(
+                    'type' => 'text',
+                    'description' => 'Site RCODE',
+                ),
+                'country' => array(
+                    'type' => 'text',
+                    'description' => 'Country of the user - will provide the list of payments
+    available for that country',
+                ),
+                'addfee' => array(
+                    'type' => 'select',
+                    'default' => array( true => 'True', false => 'False'),
+                    'description' => 'This will add the processing fees on top of the amount.',
+                ),
+                'cart' => array(
+                    'type' => 'text',
+                    'description' => 'Information about the purchase items. This is only
+    compulsory for feeapi, do not sent the cart to rawfeeapi.',
+                ),
+                'summary' => array(
+                    'type' => 'text',
+                    'description' => 'Contains a summary of cart contents',
+                ),
+                'currency_code' => array(
+                    'type' => 'text',
+                    'description' => '3 digit currency code',
+                ),
+                'items' => array(
+                    'type' => 'array',
+                    'description' => 'Contains the cart items',
+                ),
+                'name' => array(
+                    'type' => 'text',
+                    'description' => 'Category, can be used freely',
+                ),
+                'quantity' => array(
+                    'type' => 'text',
+                    'description' => 'Quantity of the item',
+                ),
+                'amount_unit' => array(
+                    'type' => 'text',
+                    'description' => 'Unit price amount (without commas and only 2 decimals
+    places)',
+                ),
+                'item_no' => array(
+                    'type' => 'text',
+                    'description' => 'Article number',
+                ),
+                'item_desc' => array(
+                    'type' => 'text',
+                    'description' => 'Item description',
+                ),
+            )
+        );
     }
 }
 ?>
--- tests/TestGateway.php
+++ tests/TestGateway.php
@@ -5,78 +5,78 @@ use Adee2210\Omnitest\Gateway;
 
 class TestGateway extends PHPUnit_Framework_TestCase
 {
-	public function TestccPurchaseSuccess()
-	{
-		$expect = '[{"mxsid":10146,"payby":"bpay","title":"BPay","display_title":null,"description":null,"transfertime":null,"transferlimit":null,"currency_code":"AUD","currency_symbol":"$","image":"--Logo URL--","fee":{"srccode":"AUD","destcode":"USD","rate":0.96801106010318,"effectivedate":"2011-04-04","flatfee":3,"mdr":1},"fields":{"card_no":{"description":"","type":"hidden","value":"70720297711","title":""},"description":{"description":"","type":"span","value":"--HTML String--"}}}]';
+    public function TestccPurchaseSuccess()
+    {
+        $expect = '[{"mxsid":10146,"payby":"bpay","title":"BPay","display_title":null,"description":null,"transfertime":null,"transferlimit":null,"currency_code":"AUD","currency_symbol":"$","image":"--Logo URL--","fee":{"srccode":"AUD","destcode":"USD","rate":0.96801106010318,"effectivedate":"2011-04-04","flatfee":3,"mdr":1},"fields":{"card_no":{"description":"","type":"hidden","value":"70720297711","title":""},"description":{"description":"","type":"span","value":"--HTML String--"}}}]';
 
         $gateway = $this->getMockBuilder('Adee2210\Omnitest\Gateway')->getMock();
-		//$gateway = new Adee2210\Omnitest;
-		$args = array(
-			'sid' => 'ST001',
-			'rcode' => 'SR10X125B',
-			'country' => 'AUS',
-			'addfee' => true,
-			'cart' => array(1=>2,2=>1), //Product ID => Quantity
-			'summary' => array('amount'=>'200.00','quantity'=>'3'),
-			'currency_code' => 'AUD',
-			'items' => array(
-				1 => array(
-					'name' => 'Mouse Logitech M510',
-					'quantity' => 2,
-					'amount_unit' => '50.00',
-					'item_no' => 'ML159',
-					'item_desc' => 'Wireless Mouse, 3-year limited hardware warranty'
-				),
-				2 => array(
-					'name' => 'Keyboard Azio PRISM',
-					'quantity' => 1,
-					'amount_unit' => '100.00',
-					'item_no' => 'MA101',
-					'item_desc' => 'USB Keyboard, 3-year limited hardware warranty'
-				),
-			),
-		);
+        //$gateway = new Adee2210\Omnitest;
+        $args = array(
+            'sid' => 'ST001',
+            'rcode' => 'SR10X125B',
+            'country' => 'AUS',
+            'addfee' => true,
+            'cart' => array(1=>2,2=>1), //Product ID => Quantity
+            'summary' => array('amount'=>'200.00','quantity'=>'3'),
+            'currency_code' => 'AUD',
+            'items' => array(
+                1 => array(
+                    'name' => 'Mouse Logitech M510',
+                    'quantity' => 2,
+                    'amount_unit' => '50.00',
+                    'item_no' => 'ML159',
+                    'item_desc' => 'Wireless Mouse, 3-year limited hardware warranty'
+                ),
+                2 => array(
+                    'name' => 'Keyboard Azio PRISM',
+                    'quantity' => 1,
+                    'amount_unit' => '100.00',
+                    'item_no' => 'MA101',
+                    'item_desc' => 'USB Keyboard, 3-year limited hardware warranty'
+                ),
+            ),
+        );
 
-		$response = $gateway->postForm('ccPurchase',$args);
+        $response = $gateway->postForm('ccPurchase',$args);
 
-		$this->assertEquals($response,$expect);
-	}
+        $this->assertEquals($response,$expect);
+    }
 
-	public function TestccPurchaseFailSummary()
-	{
-		$expect = '{"status":"EXC","msg":"Amount and Quantity didn\'t match!"}';
+    public function TestccPurchaseFailSummary()
+    {
+        $expect = '{"status":"EXC","msg":"Amount and Quantity didn\'t match!"}';
 
         $gateway = $this->getMockBuilder('Adee2210\Omnitest\Gateway')->getMock();
-		//$gateway = new Adee2210\Omnitest;
-		$args = array(
-			'sid' => 'ST001',
-			'rcode' => 'SR10X125B',
-			'country' => 'AUS',
-			'addfee' => true,
-			'cart' => array(1=>2,2=>1), //Product ID => Quantity
-			'summary' => array('amount'=>'100.00','quantity'=>'3'),
-			'currency_code' => 'AUD',
-			'items' => array(
-				1 => array(
-					'name' => 'Mouse Logitech M510',
-					'quantity' => 2,
-					'amount_unit' => '50.00',
-					'item_no' => 'ML159',
-					'item_desc' => 'Wireless Mouse, 3-year limited hardware warranty'
-				),
-				2 => array(
-					'name' => 'Keyboard Azio PRISM',
-					'quantity' => 1,
-					'amount_unit' => '100.00',
-					'item_no' => 'MA101',
-					'item_desc' => 'USB Keyboard, 3-year limited hardware warranty'
-				),
-			),
-		);
+        //$gateway = new Adee2210\Omnitest;
+        $args = array(
+            'sid' => 'ST001',
+            'rcode' => 'SR10X125B',
+            'country' => 'AUS',
+            'addfee' => true,
+            'cart' => array(1=>2,2=>1), //Product ID => Quantity
+            'summary' => array('amount'=>'100.00','quantity'=>'3'),
+            'currency_code' => 'AUD',
+            'items' => array(
+                1 => array(
+                    'name' => 'Mouse Logitech M510',
+                    'quantity' => 2,
+                    'amount_unit' => '50.00',
+                    'item_no' => 'ML159',
+                    'item_desc' => 'Wireless Mouse, 3-year limited hardware warranty'
+                ),
+                2 => array(
+                    'name' => 'Keyboard Azio PRISM',
+                    'quantity' => 1,
+                    'amount_unit' => '100.00',
+                    'item_no' => 'MA101',
+                    'item_desc' => 'USB Keyboard, 3-year limited hardware warranty'
+                ),
+            ),
+        );
 
-		$response = $gateway->postForm('ccPurchase',$args);
+        $response = $gateway->postForm('ccPurchase',$args);
 
-		$this->assertEquals($response,$expect);
-	}
+        $this->assertEquals($response,$expect);
+    }
 }
 ?>
